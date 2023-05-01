554089a5ba260f2898b195333f697d003381509e
ngtienndungg <dungnt.works@gmail.com>
2023-05-01 22:00:26 +0700

    add login ui

---
+++ app/src/main/java/com/example/social_network/feature/auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.social_network.feature.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.social_network.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
