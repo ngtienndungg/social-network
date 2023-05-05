ac78c2c591821542569d01c06714761488c0f396
ngtienndungg <dungnt.works@gmail.com>
2023-05-05 12:51:42 +0700

    config networking

---
+++ app/src/main/java/com/example/social_network/data/remote/ApiClient.java
@@ -0,0 +1,36 @@
+package com.example.social_network.data.remote;
+
+import com.example.social_network.BuildConfig;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    public static final String BASE_URL = "http://192.168.56.1/social-network-api/public/";
+    private static Retrofit retrofit;
+    public static Retrofit getRetrofit()
+    {
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        if (BuildConfig.DEBUG)
+            httpLoggingInterceptor.level(HttpLoggingInterceptor.Level.BODY);
+        else
+            httpLoggingInterceptor.level(HttpLoggingInterceptor.Level.NONE);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10,TimeUnit.SECONDS)
+                .addInterceptor(httpLoggingInterceptor).build();
+        if (retrofit == null)
+        {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
