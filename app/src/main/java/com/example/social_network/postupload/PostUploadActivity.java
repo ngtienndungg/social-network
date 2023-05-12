0b3c2c800abd4458bf0f69e729d50bc20ed97ee5
ngtienndungg <dungnt.works@gmail.com>
2023-05-12 23:48:59 +0700

    add post upload ui

---
+++ app/src/main/java/com/example/social_network/postupload/PostUploadActivity.java
@@ -0,0 +1,16 @@
+package com.example.social_network.postupload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.social_network.R;
+
+public class PostUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_upload);
+    }
+}
